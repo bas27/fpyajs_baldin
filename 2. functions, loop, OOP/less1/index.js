a8ca61c5c7b45c5643f30dc2cf32a1b2b3da6f45
Alexander B <bas2784@gmail.com>
2022-09-22 12:12:42 +0300

    modified:   2. functions, loop, OOP/less1/index.js

--- 2. functions, loop, OOP/less1/index.js
+++ 2. functions, loop, OOP/less1/index.js
@@ -1,7 +1,3 @@
-const tshirt = new Product(280, 340, "No name t-shirt", "t-shirt");
-
-const iphone = new Product(281, 60000, "Iphone 11", "mobile");
-
 function Product(id, price, title, category) {
   this.id = id,
   this.price = price,
@@ -9,13 +5,17 @@ function Product(id, price, title, category) {
   this.category = category
   };
 
-Product.prototype.getInfo = function() {
-    return `${this.category} - ${this.price}`;
+Product.prototype.getInfo = function(additional) {
+    return `${this.category} - ${this.price} - ${additional}`;
 }
+const tshirt = new Product(280, 340, "No name t-shirt", "t-shirt");
+const iphone = new Product(281, 60000, "Iphone 11", "mobile");
+// tshirt.getInfo = function() {
+//     return `${this.id} - ${this.category} - ${this.price}`;
+// }
 
-tshirt.getInfo = function() {
-    return `${this.id} - ${this.category} - ${this.price}`;
-}
+
+
 
 // tshirt.discount = 50
 
@@ -24,3 +24,26 @@ tshirt.getInfo = function() {
 
 // console.log(tshirt instanceof Object)
 
+const getInfo = tshirt.getInfo;
+
+const ctx = {
+    category: 'Toys',
+    price: 4000
+};
+// console.log(getInfo.call(ctx, 'text'))
+// console.log(getInfo.apply(ctx, ['text']))
+
+
+const arr = [15, 123, 45, 1235, 5, 45, 15, 62, 74]
+
+// console.log(Math.max(...arr))
+// console.log(Math.max.apply(null, arr))
+// console.log(Math.max(15, 123, 45, 1235, 5, 45, 15, 62, 74))
+
+function add(a, b) {
+    return a + b
+}
+
+const add10 = add.bind(null, 10);
+
+console.log(add10(8))
